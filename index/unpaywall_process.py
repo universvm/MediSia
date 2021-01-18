fab3b5f5d2213da864a9ce73861d2ff8ea277563
universvm <leonardo.castorina98@gmail.com>
2021-01-18 08:51:40 +0000

    #1 Add multiprocessing to unpaywall filter

--- index/unpaywall_process.py
+++ index/unpaywall_process.py
@@ -1,9 +1,26 @@
+import re
+import multiprocessing as mp
 from pathlib import Path
 
 import jsonlines
 import numpy as np
 
 from config import PAPERS_JSON_FOLDER, BIOPAPERS_JSON_FOLDER, BIOJOURNALS_FILE
+
+# Load Biojournals from text and normalize text:
+biojournals = np.genfromtxt(BIOJOURNALS_FILE, delimiter='\n', dtype="str").tolist()
+biojournals = [j.lower() for j in biojournals]
+biojournals = [re.sub('\W+','', j) for j in biojournals]
+
+
+def is_bio_journal(journal_dict):
+    if journal_dict["journal_name"]:
+        curr_journal = journal_dict["journal_name"].lower()
+        curr_journal = re.sub('\W+','', curr_journal)
+        if curr_journal in biojournals:
+            return journal_dict
+        else:
+            return None
 
 
 def select_and_save_biopapers(
@@ -24,20 +41,20 @@ def select_and_save_biopapers(
         Path to .txt file containing all bio-papers journals in unpaywall.
 
     """
-    biojournals = np.genfromtxt(biojournals_path, delimiter='\n', fmt="%s")
     biojournals_count = 0
+
     # Open JSONL Unpaywall file:
-    with jsonlines.open(unpaywall_path) as reader:
-        # Check if output file already exists:
-        if output_path.exists():
-            raise ValueError(f"ATTENTION: outfile {output_path} already exists. Please rename it.")
-        # If outfile does not exist
-        else:
-            # Open output:
-            with jsonlines.open(output_path, mode="a") as writer:
-                for obj in reader:
-                    if obj["journal_name"] in biojournals:
-                        writer.write(obj)
-                        biojournals_count += 1
+    with jsonlines.open(unpaywall_path) as reader, jsonlines.open(output_path, mode="a") as writer:
+        # Create Muliprocessing Pool:
+        pool = mp.Pool()
+        # Open output:
+        for ret in pool.imap(is_bio_journal, reader):
+            if ret:
+                writer.write(ret)
+                biojournals_count += 1
 
     print(f'Found {biojournals_count} Biology-related journals.')
+
+
+if __name__ == '__main__':
+    select_and_save_biopapers()
