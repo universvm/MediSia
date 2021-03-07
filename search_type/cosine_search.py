3476ae94c2bd26053402251d684edddba11a1a95
Leonardo Castorina <leonardo.castorina98@gmail.com>
2021-03-07 17:25:21 +0000

    Add sparse search

--- search_type/cosine_search.py
+++ search_type/cosine_search.py
@@ -5,13 +5,13 @@ from pathlib import Path
 
 from gensim.corpora.mmcorpus import MmCorpus
 from gensim.test.utils import get_tmpfile
-from gensim.similarities.docsim import Similarity
+from gensim.similarities.docsim import Similarity, SparseMatrixSimilarity
 
 from config import INDECES_FOLDER
 from index.tfidf_vectorizer import convert_str_to_tfidf
 
 
-def search_query_in_category(query: str, category: str, indeces_folder: Path = INDECES_FOLDER, top_k: int = 300):
+def search_query_in_category(query: str, category: str, indeces_folder: Path = INDECES_FOLDER, top_k: int = 300, sparse_search: bool = True):
     """
     Searches query in a specific category of index.
 
@@ -27,13 +27,17 @@ def search_query_in_category(query: str, category: str, indeces_folder: Path = I
     """
     tfidf_query, bow_len = convert_str_to_tfidf(query)
     # Create temporary file for similarity index
-    index_tmpfile = get_tmpfile("index")
     # Load corpora for specific category
     category_corpus_path = indeces_folder / f"{category}_corpus.mm"
     corpus = MmCorpus(str(category_corpus_path))
-    index = Similarity(index_tmpfile, corpus, num_features=bow_len)
-    # Cosine search:
-    similarity_results = index[tfidf_query]
+    if sparse_search:
+        sparse_sim = SparseMatrixSimilarity(corpus, num_features=bow_len)
+        similarity_results = sparse_sim.get_similarities(tfidf_query)
+    else:
+        index_tmpfile = get_tmpfile("index")
+        index = Similarity(index_tmpfile, corpus, num_features=bow_len)
+        # Cosine search:
+        similarity_results = index[tfidf_query]
     # Sort by most relevant:
     sorted_docid_results = sorted(range(len(similarity_results)), key=lambda k: similarity_results[k], reverse=True)[:top_k]
     # Import metadata
