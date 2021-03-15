db10f02b6677cc7eff430dd955abba4eb05e2c61
Leonardo Castorina <leonardo.castorina98@gmail.com>
2021-03-15 10:10:53 +0000

    #22 Add normal similarity search

--- search_type/search_module.py
+++ search_type/search_module.py
@@ -1,4 +1,5 @@
 import linecache
+import time
 import warnings
 import typing as t
 import multiprocessing as mp
@@ -10,12 +11,18 @@ import joblib
 from gensim.corpora.mmcorpus import MmCorpus
 from sklearn.preprocessing import LabelEncoder
 from gensim.matutils import sparse2full
+from gensim.test.utils import get_tmpfile
 from gensim.similarities.docsim import Similarity, SparseMatrixSimilarity
 
 
 from config import INDECES_FOLDER, QUERY_CLASSIFIER, BOW_LENGTH
 from index.tfidf_vectorizer import convert_str_to_tfidf
 from index.unpaywall_process import build_journal_category_dict
+
+# Turning off gensim printouts:
+import logging
+
+logging.getLogger("gensim").setLevel(logging.WARNING)
 
 
 class SearchModule:
@@ -40,7 +47,7 @@ class SearchModule:
         self.categories = sorted(categories_list)
 
         self.classifier_categories = copy.copy(self.categories)
-        self.classifier_categories.remove("nutrition")                                       
+        self.classifier_categories.remove("nutrition")
         self.label_encoder = LabelEncoder()
         # Fit categories to label encoder
         self.label_encoder.fit(self.categories)
@@ -96,24 +103,41 @@ class SearchModule:
         # Load corpora for specific category
         category_corpus_path = self.indeces_folder / f"{category}_corpus.mm"
         corpus = MmCorpus(str(category_corpus_path))
-        sparse_sim = SparseMatrixSimilarity(
-            corpus, num_features=self.num_features, num_best=self.top_k
-        )
-        similarity_results = sparse_sim.get_similarities(tfidf_query)
+        if self.sparse_search:
+            sparse_sim = SparseMatrixSimilarity(
+                corpus, num_features=self.num_features, num_best=self.top_k
+            )
+            similarity_results = sparse_sim.get_similarities(tfidf_query)
+        else:
+            index_tmpfile = get_tmpfile("index")
+            index = Similarity(
+                index_tmpfile,
+                corpus,
+                num_features=self.num_features,
+                num_best=self.top_k,
+            )
+            # Cosine search:
+            similarity_results = index[tfidf_query]
         # Sort by most relevant:
         sorted_docid_results = sorted(
             range(len(similarity_results)),
             key=lambda k: similarity_results[k],
             reverse=True,
-        )[:self.top_k]
+        )[: self.top_k]
         metadata = linecache.getlines(str(self.cat_to_cache_dict[category]))
 
         return zip(sorted_docid_results, itemgetter(*sorted_docid_results)(metadata))
 
     def classify_query(self, tfidf_query, top_cat: int = 4):
         full_tfidf_query = sparse2full(tfidf_query, length=self.num_features)
-        query_scores = self.query_classifier.predict_proba(full_tfidf_query.reshape(1, -1))
-        sorted_query = sorted(zip(self.classifier_categories, query_scores[0]), key=lambda x: x[1], reverse=True)
+        query_scores = self.query_classifier.predict_proba(
+            full_tfidf_query.reshape(1, -1)
+        )
+        sorted_query = sorted(
+            zip(self.classifier_categories, query_scores[0]),
+            key=lambda x: x[1],
+            reverse=True,
+        )
         # Convert topn cat into numbers:
         query_category = [(tfidf_query, cat) for cat, score in sorted_query[:top_cat]]
         return query_category
@@ -121,5 +145,7 @@ class SearchModule:
 
 if __name__ == "__main__":
     search_module = SearchModule()
+    start = time.time()
     results = search_module.search("coronavirus")
-    print(results)
+    end = time.time()
+    print(end - start)
